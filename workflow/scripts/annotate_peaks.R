97b96aea5d2490d6f6b7d62ad80a1f84028d1ace
niekwit <41302859+niekwit@users.noreply.github.com>
2026-04-08 10:25:17 +0100

    proper loading of txdb

--- workflow/scripts/annotate_peaks.R
+++ workflow/scripts/annotate_peaks.R
@@ -11,15 +11,17 @@ library(tidyverse)
 # Load Snakemake parameters
 bed_file <- snakemake@input[["bed"]]
 edb_file <- snakemake@input[["edb"]]
-peak.mode <- snakemake@params[["pm"]]
+txdb_file <- snakemake@input[["txdb"]]
 txt <- snakemake@output[["txt"]]
-gtf <- snakemake@params[["gtf"]]
-
-# Load annotation database
-txdb <- makeTxDbFromGFF(gtf)
 
 # Annotate bed file
-peakAnno <- annotatePeak(bed_file, tssRegion = c(-3000, 3000), TxDb = txdb)
+txdb <- AnnotationDbi::loadDb(txdb_file)
+peaks <- readPeakFile(bed_file, as = "GRanges")
+seqlevels(peaks, pruning.mode = "coarse") <- intersect(
+  seqlevels(peaks),
+  seqlevels(txdb)
+)
+peakAnno <- annotatePeak(peaks, tssRegion = c(-3000, 3000), TxDb = txdb)
 
 # Tidy up annotation data
 df <- as.data.frame(peakAnno@anno@elementMetadata@listData)
@@ -28,7 +30,7 @@ names(df)[1:3] <- c("peak_id", "fold_enrichment", "strand")
 # Add gene names and gene biotype to annotation
 load(edb_file)
 df <- df %>%
-    left_join(edb, by = "geneId")
+  left_join(edb, by = "geneId")
 
 # Write annotation to file
 write.table(df, file = txt, quote = FALSE, sep = "\t", row.names = FALSE)
